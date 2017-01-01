d944cd22d7c38f694a67ed2c0d6a8b5b0abef3de
Madhur Malhotra <malhotra.madhur15@gmail.com>
2016-12-31 23:45:00 -0600

    added background color, initial frame test works

--- view-challenges.playground/Pages/challenge4.xcplaygroundpage/Contents.swift
+++ view-challenges.playground/Pages/challenge4.xcplaygroundpage/Contents.swift
@@ -20,6 +20,6 @@ let colors: [UIColor] = [.green, .gray, .purple, .yellow, .red, .orange, .lightG
  */
 let challengeFourFrame1 = CGRect(x: 0.0, y: 0.0, width: 99.0, height: 99.0)
 let challengeFourView1 = UIView(frame: challengeFourFrame1)
-
+challengeFourView1.backgroundColor = .white
 
 
