34ebead00ae133314694ffdd072b4a907cbda9a1
Madhur Malhotra <malhotra.madhur15@gmail.com>
2017-01-01 23:42:51 -0600

    added subview9 for challenge 4

--- view-challenges.playground/Pages/challenge4.xcplaygroundpage/Contents.swift
+++ view-challenges.playground/Pages/challenge4.xcplaygroundpage/Contents.swift
@@ -53,3 +53,7 @@ challengeFourView8.backgroundColor = .black
 let challengeFourFrame9 = CGRect(x: 0.0, y: 0.0, width: 33.0, height: 33.0)
 let challengeFourView9 = UIView(frame: challengeFourFrame9)
 challengeFourView9.backgroundColor = .white
+
+let challengeFourFrame5 = CGRect(x: 0.0, y: 0.0, width: 33.0, height: 33.0)
+let challengeFourView5 = UIView(frame: challengeFourFrame5)
+challengeFourView5.backgroundColor = .yellow
